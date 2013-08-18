71fa7cba8b37774f6f03d545e677d09eaa5a95e3
ironic <faopera@gmail.com>
2013-08-18 13:04:56 +0300

    python 3 e tasima

--- pykapat.py
+++ pykapat.py
@@ -1,15 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-# GPL 3
-# Copyright 2011-2012 Fatih Bostancı <faopera@gmail.com>
-# bağımlılıklar: python 2, python-dbus
+# Copyright (c) 2011-2013 Fatih Bostancı <faopera@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
+# bağımlılıklar: python 3, python-dbus
 
 # TODO: qt (ve ? gtk) arayüz
 # TODO: kapatma başlamadan ekrana işlemi iptal etme penceresi
 # TODO: dbus python 3 e geçince uygulamayı python3 e taşıma.
 
-import getopt
+import getopt, sys
 from dbus import SystemBus,Interface
 from sys import exit,argv,stdout
 from time import strftime,sleep
@@ -20,14 +36,14 @@ def bekleme_goster(bekleme_suresi):
         bekleme_suresi -= 1
 
         while bekleme_suresi > 0:
-            print '\r', '\033[;1mBilgisayarınız \033[1;33;44m',bekleme_suresi / 3600, 'saat', \
-                    bekleme_suresi % 3600 / 60, 'dakika', bekleme_suresi % 60, 'saniye', \
-                    '\033[;1m sonra kapatılacak...\033[0m',
-            stdout.flush()
+            print("\033[;1mBilgisayarınız \033[1;33;44m",int(bekleme_suresi / 3600), "saat", \
+                  int(bekleme_suresi % 3600 / 60), "dakika", int(bekleme_suresi % 60), "saniye", \
+                  "\033[;1m sonra kapatılacak...\033[0m",end="\r")
+           # sys.stdout.flush()
             bekleme_suresi -= 1
             sleep(1)
     except KeyboardInterrupt:
-        print "\n\033[1;41mKullanıcı tarafından işlem iptal edildi !!!\033[0m"
+        print("\n\033[1;41mKullanıcı tarafından işlem iptal edildi !!!\033[0m")
         exit(1)
 
 def simdi_kapat(istek):
@@ -37,20 +53,26 @@ def simdi_kapat(istek):
     dbus_kapat = Interface(dbus_k, 'org.freedesktop.ConsoleKit.Manager')
 
     if istek == 'Stop':
-        print "\033[1;41mBilgisayarınız kapatılıyor...\033[0m"
+        print("\033[1;41mBilgisayarınız kapatılıyor...\033[0m")
         dbus_kapat.Stop()
     else:
-        print "\033[1;41mBilgisayarınız yeniden başlatılıyor...\033[0m"
+        print("\033[1;41mBilgisayarınız yeniden başlatılıyor...\033[0m")
         dbus_kapat.Restart()
 
+#def simdi_askiya_al(istek):
+#    bus = SystemBus()
+#    dbus_as = bus.get_object('org.freedesktop.UPower',
+#                            '/org/freedesktop/UPower')
+#    dbus_askiya_al = Interface(dbus_as, 'org.freedesktop.UPower')
+#    dbus_askiya_al.Suspend()
 
 class PyKapat:
     def main(self):
         try:
-            opts, args = getopt.gnu_getopt(argv[1:], "hvkys:d:",
+            opts, args = getopt.gnu_getopt(sys.argv[1:], "hvkys:d:",
                         ["help", "version", "kapat", "yeniden-baslat", "saat=", "dakika="])
-        except getopt.GetoptError, err:
-            print "pykapat: %s" % (str(err))
+        except getopt.GetoptError as err:
+            print("pykapat: %s" % (str(err)))
             exit(2)
 
         for o, a in opts:
@@ -67,18 +89,18 @@ class PyKapat:
             elif o in ("-h", "--help"):
                 kapat.kullanim()
             else:
-                assert False, "Geçersiz değişken"
+                assert False, "Yakalanamayan seçenek"
 
 
     def dakika_sonra(self, dakika):
         try:
             self.dakika = int(dakika)
         except ValueError:
-            print "\033[1;41mDakika olarak sayısal bir değer giriniz.\033[0m"
+            print("\033[1;41mDakika olarak sayısal bir değer giriniz.\033[0m")
             exit(1)
 
         if int(self.dakika) < 0:
-            print "\033[1;41mGirilen dakika 0'dan küçük olamaz.\033[0m"
+            print("\033[1;41mGirilen dakika 0'dan küçük olamaz.\033[0m")
             exit(1)
 
         self.dakika_bekleme_suresi = int(self.dakika) * 60
@@ -87,41 +109,34 @@ class PyKapat:
 
     def su_saatte(self, saat):
         self.saat = saat
-        if len(self.saat) > 5:
-            print "\033[1;1mÇok fazla değişken:\033[1;33m %s\033[0m" % (self.saat)
-            exit(1)
-
-        elif len(self.saat) < 4:
-            print "\033[1;1meksik değişken:\033[1;33m %s\033[0m" % (self.saat)
-            exit(1)
-
         saat_ayir = self.saat.split(":", 1)
+
         try:
             int(saat_ayir[0])
             int(saat_ayir[1])
 
         except ValueError:
-            print "\033[1;41mSaat olarak sayısal bir değer giriniz.\033[0m"
+            print("\033[1;41mSaat olarak sayısal bir değer giriniz.\033[0m")
             exit(1)
 
         except IndexError:
-            print "\033[1;41mSaat ve dakika arasında ':' olmalı.\033[0m"
+            print("\033[1;41mSaat ve dakika arasında ':' olmalı.\033[0m")
             exit(1)
 
         if int(saat_ayir[0]) > 23:
-            print "\033[1;41mGirilen saat en fazla 23 olabilir.\033[0m"
+            print("\033[1;41mGirilen saat en fazla 23 olabilir.\033[0m")
             exit(1)
 
         elif int(saat_ayir[0]) < 0:
-            print "\033[1;41mGirilen saat 0'dan küçük olamaz.\033[0m"
+            print("\033[1;41mGirilen saat 0'dan küçük olamaz.\033[0m")
             exit(1)
 
         if int(saat_ayir[1]) > 59:
-            print "\033[1;41mGirilen dakika en fazla 59 olabilir.\033[0m"
+            print("\033[1;41mGirilen dakika en fazla 59 olabilir.\033[0m")
             exit(1)
 
         elif int(saat_ayir[1]) < 0:
-            print "\033[1;41mGirilen dakika 0'dan küçük olamaz.\033[0m"
+            print("\033[1;41mGirilen dakika 0'dan küçük olamaz.\033[0m")
             exit(1)
 
 
@@ -130,28 +145,28 @@ class PyKapat:
 
         if self.girilen_saat_saniye >= self.simdiki_saat_saniye:
             self.saat_bekleme_suresi = self.girilen_saat_saniye - self.simdiki_saat_saniye
-            print "\033[1;1mBilgisayarınızın kapatılacağı saat:\033[1;33m %s\033[0m" % (self.saat)
+            print("\033[1;1mBilgisayarınızın kapatılacağı saat:\033[1;33m %s\033[0m" % (self.saat) )
             bekleme_goster(self.saat_bekleme_suresi)
             simdi_kapat('Stop')
 
         elif self.girilen_saat_saniye < self.simdiki_saat_saniye:
             self.saat_bekleme_suresi = 24 * 3600 - self.simdiki_saat_saniye + self.girilen_saat_saniye
-            print "\033[1;1mBilgisayarınızın kapatılacağı saat:\033[1;33m %s (Yarın)\033[0m" % (self.saat)
+            print("\033[1;1mBilgisayarınızın kapatılacağı saat:\033[1;33m %s (Yarın)\033[0m" % (self.saat))
             bekleme_goster(self.saat_bekleme_suresi)
             simdi_kapat('Stop')
 
     def surum_bilgisi(self):
-        print "PyKapat v2.0"
+        print("PyKapat v0.3.0")
 
     def kullanim(self):
-        print """
+        print("""
         -s, --saat <ss:dd>       : Girilen saatte bilgisayarı kapatır.
         -d, --dakika <dakika>    : Girilen dakika sonra bilgisayarı kapatır.
         -k, --kapat              : Bilgisayarı hemen kapatır.
         -y, --yeniden-baslat     : Bilgisayarı hemen yeniden başlatır.
         -v, --version            : Uygulamanın sürümünü görüntüler.
         -h, --help               : Bu yardım çıktısını görüntüler.
-        """
+        """)
 
 kapat = PyKapat()
 
@@ -161,6 +176,10 @@ if __name__ == "__main__":
 
 ###Sürüm Geçmişi Başlangıç###
 #
+# v0.3.0
+# * Python 3'e taşınma
+#
+#------------------------------------------------
 # v0.2.0
 # * Renkli çıktılar
 # * Terminalde geri sayım sayacı (saat,dakika,saniye olarak) gösterme
